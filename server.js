979abe8cf5562084347589c9c6427bfb07418981
Ido-Geminer <idogeminer@gmail.com>
2024-08-31 13:25:02 +0300

    finished project

--- server.js
+++ server.js
@@ -7,17 +7,16 @@ const cheerio = require("cheerio");
 let stockTicker = "MRNA";
 let type = "history";
 
-async function scrapeData() {
+async function scrapeData(ticker) {
   try {
     //step a - fetch the html page & getting the html result
-    const url = `https://finance.yahoo.com/quote/${stockTicker}/${type}/`;
+    const url = `https://finance.yahoo.com/quote/${ticker}/${type}?p=${ticker}`;
     const res = await fetch(url);
     const html = await res.text();
-    console.log(html);
-
     const $ = cheerio.load(html);
     const price_history = getPrices($);
     console.log(price_history);
+    return price_history;
   } catch (err) {
     console.log(err.message);
   }
@@ -31,7 +30,31 @@ function getPrices(cher) {
     });
   return prices;
 }
-scrapeData();
+
 //step 2 - initialize server that serves up an html file that the user can play with
 
+//loading the express library
+const express = require("express");
+//invoking express on port 8383
+const app = express();
+const port = 8383;
+
+//listen to incoming requests on port 8383
+app.listen(port, () => console.log(`Server has started on port: ${port}`));
+
+//middleware - settings of the server
+app.use(express.json());
+//enabling cors
+app.use(require("cors")());
+//when someone accesses our server on the browser its going to give them the index.html file in the public folder
+app.use(express.static("public"));
+
 //step 3 - define api endpoints to access stock data (and call webscaper)
+
+//defining endpoints
+app.post(`/api`, async (req, res) => {
+  const { stock_ticker: ticker } = req.body;
+  console.log(ticker);
+  const prices = await scrapeData(ticker);
+  res.status(200).send({ prices });
+});
